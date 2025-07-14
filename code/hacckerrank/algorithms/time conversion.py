f5a9c247b720143e426fc3dcfa70555184c51d85
Tiendzok <nvtien163@gmail.com>
2025-07-14 23:45:44 +0700

    add 1 hackerrank

---
+++ code/hacckerrank/algorithms/time conversion.py
@@ -0,0 +1,39 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    hh = int(s[:2])
+    rest = s[2:8]
+    period = s[8:]
+
+    if (period == "AM"):
+        if hh == 12:
+            hh = 0
+    else:
+        if hh != 12:
+            hh += 12
+    return f"{hh:02d}{rest}"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
